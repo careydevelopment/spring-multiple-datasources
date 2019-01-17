4668f329b79b3fbfc7423d94bfcf6a65625cfa01
Brian Carey <mrbrianmcarey@gmail.com>
2019-01-17 16:16:06 -0500

    initial checkin

---
+++ src/main/java/com/careydevelopment/multidatasource/CustomerManagementApplication.java
@@ -0,0 +1,12 @@
+package com.careydevelopment.multidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementApplication {
+    
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagementApplication.class,args);
+	}
+}
